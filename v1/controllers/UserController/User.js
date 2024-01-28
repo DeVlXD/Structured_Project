cf1e2e2263c22e9c8ce87448384215015b8af38e
Nitish Verma <nitish.verma2001@gmail.com>
2024-01-28 18:35:18 +0530

    added jwt

--- v1/controllers/UserController/User.js
+++ v1/controllers/UserController/User.js
@@ -1,85 +1,126 @@
-const asyncHandler = require("express-async-handler");
+// const asyncHandler = require("express-async-handler");
+const constants = require("../../../common/constants.js");
 const Model = require("../../../models/Index");
 const User = Model.User;
+const validate = require("../../validations/userValidationSchema.js");
+const bcrypt = require("bcryptjs");
+const Auth = require("../../../common/authenticate");
 
-const registerUser = asyncHandler(async (req, res) => {
-    const {username, email, password} = req.body;
-    if(!username || !email || !password){
-        res.status(400);
-        throw new Error("All fields are mandatory!");
+module.exports.registerUser = async (req, res, next) => {
+    try {
+        await validate.validateUser.validateAsync(req.body);
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            throw new Error("All fields are mandatory!");
+        }
+        const userAvailable = await User.findOne({ email });
+        if (userAvailable) {
+            throw new Error("User already registered!")
+        }
+
+        //Hash password
+        const hashPassword = await bcrypt.hash(password, 10);
+        // console.log("hashed password: ", hashPassword);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashPassword
+        });
+        // console.log(`User created ${user}`);
+        if (user) {
+            console.log(`User created ${user}`);
+            return res.json({ _id: user.id, email: user.email, message: "Register the user" });
+        } else {
+            throw new Error("User data is not valid");
+        }
+    } catch (error) {
+        next(error);
     }
-    const userAvailable = await User.findOne({email});
-    if(userAvailable){
-        res.status(400);
-        throw new Error("User already registered!")
+};
+
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            res.status(400);
+            throw new Error("All fields are mandatory!");
+        }
+        const user = await User.findOne({email});
+        //compare password with hashed password
+        if(user && (await bcrypt.compare(password, user.password))){
+            const accessToken = await Auth.getToken({
+                _id: user._id,
+                role: "user",
+                jti: user.jti,
+                secretId: req.headers.deviceId
+            })
+            res.status(200).json({accessToken});
+        } else{
+        res.status(401);
+        throw new Error(constants.INVALID_CREDENTIALS);
     }
-    const user = await User.create({
-        username,
-        email, 
-        password
-    });
-    console.log(`User created ${user}`);
-    if(user){
-        res.status(201).json({_id: user.id, email: user.email});
-    } else{
-        res.status(400);
-        throw new Error("User data is not valid");
+    } catch (error) {
+        next(error);
     }
-    res.json({message: "Register the user"});
-});
+};
 
-const loginUser = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    if(!email || !password){
-        res.status(400);
-        throw new Error("All fields are mandatory!");
-    } else{
-        res.status(200);
-        res.json({message: "Login succesful!"});
+
+module.exports.getUser = async (req,res, next) => {
+    try {
+        await validate.validateId.validateAsync(req.params);
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return res.json(user);
+    } catch (error) {
+        next(error);
     }
-});
+};
 
-const getUser = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      res.status(404);
-      throw new Error("User not found");
+module.exports.getUsers = async (req,res, next) => {
+    try {
+        const users = await User.find();
+        return res.json(users);
+    } catch (error) {
+        next(error);
     }
-    res.status(200).json(user);
-});
+};
 
-const getUsers = asyncHandler(async (req, res) => {
-    const users = await User.find();
-    res.status(200).json(users);
-});
 
-const updateUser = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if(!user){
-        res.status(404);
-        throw new Error("User not found!");
+module.exports.updateUser = async (req,res, next) => {
+    try {
+        await validate.validateUpdate.validateAsync(req.body);
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            throw new Error("User not found!");
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        return updatedUser;
+    } catch (error) {
+        next(error);
     }
+};
 
-    const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new: true}
-    );
-    res.status(200).json(updatedUser);
-});
+module.exports.deleteUser = async (req, res, next) => {
+    try {
+        await validate.validateId.validateAsync(req.params);
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            throw new Error("User not found!");
+        }
 
-const deleteUser = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if(!user){
-        res.status(404);
-        throw new Error("User not found!");
+        await User.deleteOne({ _id: req.params.id });
+
+        return user;
+    } catch (error) {
+        next(error);
     }
-
-    await User.deleteOne();
-
-    res.status(200).json(user);
-});
-
-// dbconnect();
-
-module.exports = {getUser, loginUser, updateUser, deleteUser, registerUser, getUsers};
+};
--- v1/routes/Upload.js
+++ v1/routes/Upload.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const uploadService = require("../../services/UploadServices");
 const Controller = require('../controllers');
 
-router.post('/uploadFile', uploadService.upload.array('file'), Controller.UploadController.uploadFile);
+router.post('/uploadfile', uploadService.upload.array('file'), Controller.UploadController.uploadFile);
 
 
 module.exports = router;
